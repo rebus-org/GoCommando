ecc65d135af14a4b35d98e6cdbe742c1b50eac92
Mogens Heller Grabe <mookid8000@gmail.com>
2011-06-21 13:21:39 +0200

    converted to VS2010

--- src/GoCommando/Exceptions/CommandoException.cs
+++ src/GoCommando/Exceptions/CommandoException.cs
@@ -5,7 +5,12 @@ namespace GoCommando.Exceptions
     public class CommandoException : ApplicationException
     {
         public CommandoException(string message, params object[] objs)
-            : base(String.Format(message, objs))
+            : base(string.Format(message, objs))
+        {
+        }
+
+        public CommandoException(Exception innerException, string message, params object[] objs)
+            : base(string.Format(message, objs), innerException)
         {
         }
     }
