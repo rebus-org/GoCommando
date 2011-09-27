144929024f2f00aed3d2ac7ae17919eeb3e4412a
unknown <PowerHub Developer@.ph.de-prod.dk>
2011-09-27 08:29:35 +0200

    first attempt at creating a NuGet package

--- src/GoCommando/Properties/AssemblyInfo.cs
+++ src/GoCommando/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GoCommando")]
-[assembly: AssemblyDescription("Command line utility helper")]
-[assembly: AssemblyCompany("Mogens Heller Grabe, mookid8000@gmail.com, @mookid8000")]
+[assembly: AssemblyDescription("GoCommando is a small command line utility helper that does the boring work when creating command line utilities in .NET.")]
+[assembly: AssemblyCompany("Mogens Heller Grabe")]
 [assembly: AssemblyProduct("GoCommando")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a4168feb-47c2-4fcc-93f8-e14172192ec3")]
 
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
