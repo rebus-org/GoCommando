7d7a2552e1301a3c76aa58f3a5b6030acc4e78fe
Mogens Heller Grabe <mookid8000@gmail.com>
2015-10-07 09:35:41 +0200

    better script

--- build.cmd
+++ build.cmd
@@ -1,2 +1,40 @@
+@echo off
+
+echo GoCommand build thing.
+echo.
+
+set buildVersion=%1
+
+set outputPath="%~dp0\deploy"
+
+if exist "%outputPath%" (
+	rd "%outputPath%" /s/q
+)
+
+mkdir "%outputPath%"
+
+if "%buildVersion%"=="" (
+	echo.
+	echo Please specify which version to build as an argument
+	echo.
+	goto exit
+)
+
+echo Building version %buildVersion%...
+
 msbuild GoCommando\GoCommando.csproj -P:Configuration=Release
-nuget\nuget pack Package.nuspec -out c:\temp
+
+echo Tagging...
+
+git tag %buildVersion%
+
+echo Packing...
+
+nuget\nuget pack Package.nuspec -out %outputPath% -version %buildVersion%
+
+echo Pushing...
+
+git push
+git push --tags
+
+:exit
