eca545f359b9c7ef81e89139aa0715bb04d9c8e5
Mogens Heller Grabe <mookid8000@gmail.com>
2015-10-08 11:49:24 +0200

    added test command

---
+++ TestApp/Commands/NewCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GoCommando;
+
+namespace TestApp.Commands
+{
+    [Command("new")]
+    public class NewCommand : ICommand
+    {
+        [Parameter("qid")]
+        public string QueryId { get; set; }
+
+        public void Run()
+        {
+            Console.WriteLine($"QueryId {QueryId}");   
+        }
+    }
+}
