0763d4461c91b5277ca8a64a8bd60423e96d4eb0
Mads Tjørnelund Toustrup <mtt@d60.dk>
2019-05-23 15:17:29 +0200

    Added CoreTestApp for netcore validation.

---
+++ CoreTestApp/Commands/GuidCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using GoCommando;
+
+namespace CoreTestApp.Commands
+{
+    [Command("new")]
+    [Description("Generates a new guid")]
+    public class GuidCommand : ICommand
+    {
+        public void Run()
+        {
+            Console.WriteLine(Guid.NewGuid());
+        }
+    }
+}
---
+++ CoreTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GoCommando;
+
+namespace CoreTestApp
+{
+    [Banner(
+@"---------
+This is actually a netcore application using GoCommando...
+---------
+")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Go.Run();
+        }
+    }
+}
