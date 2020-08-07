efba30a3be7def33732997d2b49b23d5c5398e54
Mogens Heller Grabe <mookid8000@gmail.com>
2020-08-07 09:48:37 +0200

    add things

---
+++ Beverage/Program.cs
@@ -0,0 +1,18 @@
+﻿using GoCommando;
+
+namespace Beverage
+{
+    [Banner(@"------------------------------
+Beverage Utility
+
+Copyright (c) 2015 El Duderino
+------------------------------")]
+    [SupportImpersonation]
+    class Program
+    {
+        static void Main()
+        {
+            Go.Run();
+        }
+    }
+}
